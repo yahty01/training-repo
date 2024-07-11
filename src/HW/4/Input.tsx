3b12cb4d300568cc2a217512469d9402cc6b0599
Egor Savelev <119876639+yahty01@users.noreply.github.com>
2024-07-11 05:12:50 +0300

    hw 4

--- src/HW/4/Input.tsx
+++ src/HW/4/Input.tsx
@@ -1,14 +1,20 @@
+import {ChangeEvent} from "react";
+
 type InputPropsType = {
-	currentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-	setCurrentText: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+	currentText: string // НУЖНО ПРОТИПИЗИРОВАТЬ
+	setCurrentText: (s: string) => void // НУЖНО ПРОТИПИЗИРОВАТЬ
+	handleSave: () => void
 };
 
 export const Input = (props: InputPropsType) => {
-	const onChangeHandler = (event: 'НУЖНО ПРОТИПИЗИРОВАТЬ') => {
-		// НУЖНО ДОПИСАТЬ
-	};
+	const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+		props.setCurrentText(event.currentTarget.value)	};
 
-	// return (
-	//   <input id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
-	// );
+	return (
+	  <input onKeyUp={event => {
+		  if (event.key === 'Enter') {
+			  props.handleSave()
+		  }
+	  }} id={'hw04-input'} type="text" value={props.currentText} onChange={onChangeHandler} />
+	);
 };
