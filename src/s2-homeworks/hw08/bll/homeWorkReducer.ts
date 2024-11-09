7eadb0bb081e03d7fa374949643eb9c2de627c7c
yahty01 <yahty01@yandex.ru>
2024-11-09 22:11:04 +0300

    fix(hw8) done

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,36 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort': {
+            if (action.payload === 'up') {
+                return [...state].sort(function (a, b) {
+                    if (a.name < b.name) {
+                        return -1;
+                    }
+                    if (a.name > b.name) {
+                        return 1;
+                    }
+                    return 0;
+                })
+            }
 
-            return state // need to fix
+            if (action.payload === 'down') {
+                return [...state].sort(function (a, b) {
+                    if (a.name < b.name) {
+                        return 1;
+                    }
+                    if (a.name > b.name) {
+                        return -1;
+                    }
+                    return 0;
+                })
+            }
+            return state
         }
         case 'check': {
-
-            return state // need to fix
+            return [...state].filter(user => user.age >= 18)
         }
         default:
             return state
