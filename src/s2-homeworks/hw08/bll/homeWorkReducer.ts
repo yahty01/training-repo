cd80de0345ee4b1984e06f8f4a08f7833d2f6e41
Egor Savelev <119876639+yahty01@users.noreply.github.com>
2024-11-12 18:09:03 +0300

    fit(hw8) refactor homeWorkReducer and jest tests

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -9,14 +9,16 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
 		case 'sort': {
 			const newState = [...state].sort(function (a, b) {
 				if (a.name < b.name) return -1;
-				if (a.name > b.name) return 1;
+				else if (a.name > b.name) return 1;
 				else return 0;
 			})
 			return action.payload === 'up' ? newState : newState.reverse();
 		}
+
 		case 'check': {
 			return state.filter(user => user.age >= action.payload)
 		}
+
 		default:
 			return state
 	}
