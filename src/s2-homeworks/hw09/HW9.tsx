2581a58b307c760cf8013ef8dbdd2b04f6170251
Egor Savelev <119876639+yahty01@users.noreply.github.com>
2024-11-12 22:36:37 +0300

    fit(hw9) done

--- src/s2-homeworks/hw09/HW9.tsx
+++ src/s2-homeworks/hw09/HW9.tsx
@@ -4,8 +4,8 @@ import s2 from '../../s1-main/App.module.css'
 
 /*
 * 1 - в файле Clock.tsx дописать функции stop, start, onMouseEnter, onMouseLeave
-* 2 - в файле Clock.tsx из переменной date вычислить значения stringTime, stringDate, stringDay, stringMonth
-* 3 - в файле Clock.tsx дизэйблить кнопки стоп / старт если таймер не запущен / запущен соответственно
+* 2 - ✅в файле Clock.tsx из переменной date вычислить значения stringTime, stringDate, stringDay, stringMonth
+* 3 - ✅в файле Clock.tsx дизэйблить кнопки стоп / старт если таймер не запущен / запущен соответственно
 * 4 - сделать стили в соответствии с дизайном
 * */
 
